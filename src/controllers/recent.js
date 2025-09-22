a3b34608de3ea8f7ac49e43358435d73ce3e6661
Moza Al Fahad <mfahad@andrew.cmu.edu>
2025-09-22 11:28:19 +0000

    My changes from 1B

--- src/controllers/recent.js
+++ src/controllers/recent.js
@@ -1,37 +1,53 @@
-
 'use strict';
 
 const nconf = require('nconf');
 
-const user = require('../user');
-const topics = require('../topics');
-const meta = require('../meta');
-const helpers = require('./helpers');
-const pagination = require('../pagination');
-const privileges = require('../privileges');
-
 const recentController = module.exports;
 const relative_path = nconf.get('relative_path');
+
+// helper to set title & breadcrumbs
+function setTitleAndBreadcrumbs(data, url, asHome) {
+	if (asHome) {
+		data.title = meta.config.homePageTitle || '[[pages:home]]';
+		return;
+	}
+	data.title = `[[pages:${url}]]`;
+	data.breadcrumbs = helpers.buildBreadcrumbs([{ text: `[[${url}:title]]` }]);
+}
+
+// helper to set RSS fields
+function setRssFields(ctx) {
+	const { data, url, req, rssToken } = ctx;
+	const disabled = meta.config['feeds:disableRSS'] || 0;
+
+	data['feeds:disableRSS'] = disabled;
+	if (disabled) return;
+
+	let rss = `${relative_path}/${url}.rss`;
+	if (req.loggedIn) {
+		rss += `?uid=${req.uid}&token=${rssToken}`;
+	}
+	data.rssFeedUrl = rss;
+}
 
 recentController.get = async function (req, res, next) {
 	const data = await recentController.getData(req, 'recent', 'recent');
 	if (!data) {
 		return next();
 	}
-
 	res.render('recent', data);
 };
 
 recentController.getData = async function (req, url, sort) {
 	const page = parseInt(req.query.page, 10) || 1;
-	let term = helpers.terms[req.query.term];
+
+	// single, simplified term logic
+	const termKey = req.query.term;
+	let term = termKey ? helpers.terms[termKey] : 'alltime';
+	if (termKey && !term) return null;
+
 	const { cid, tag } = req.query;
 	const filter = req.query.filter || '';
-
-	if (!term && req.query.term) {
-		return null;
-	}
-	term = term || 'alltime';
 
 	const [settings, categoryData, tagData, rssToken, canPost, isPrivileged] = await Promise.all([
 		user.getSettings(req.uid),
@@ -49,27 +65,23 @@ recentController.getData = async function (req, url, sort) {
 		cids: cid,
 		tags: tag,
 		uid: req.uid,
-		start: start,
-		stop: stop,
-		filter: filter,
-		term: term,
-		sort: sort,
+		start,
+		stop,
+		filter,
+		term,
+		sort,
 		floatPinned: req.query.pinned,
 		query: req.query,
 	});
 
-	const isDisplayedAsHome = !(req.originalUrl.startsWith(`${relative_path}/api/${url}`) || req.originalUrl.startsWith(`${relative_path}/${url}`));
-	const baseUrl = isDisplayedAsHome ? '' : url;
-
-	if (isDisplayedAsHome) {
-		data.title = meta.config.homePageTitle || '[[pages:home]]';
-	} else {
-		data.title = `[[pages:${url}]]`;
-		data.breadcrumbs = helpers.buildBreadcrumbs([{ text: `[[${url}:title]]` }]);
-	}
+	// use one computation + helper
+	const asHome = !(req.originalUrl.startsWith(`${relative_path}/api/${url}`) || req.originalUrl.startsWith(`${relative_path}/${url}`));
+	const baseUrl = asHome ? '' : url;
+	setTitleAndBreadcrumbs(data, url, asHome);
 
 	const query = { ...req.query };
 	delete query.page;
+
 	data.canPost = canPost;
 	data.showSelect = isPrivileged;
 	data.showTopicTools = isPrivileged;
@@ -78,29 +90,15 @@ recentController.getData = async function (req, url, sort) {
 	data.selectedCids = categoryData.selectedCids;
 	data.selectedTag = tagData.selectedTag;
 	data.selectedTags = tagData.selectedTags;
-	data['feeds:disableRSS'] = meta.config['feeds:disableRSS'] || 0;
-	if (!meta.config['feeds:disableRSS']) {
-		data.rssFeedUrl = `${relative_path}/${url}.rss`;
-		if (req.loggedIn) {
-			data.rssFeedUrl += `?uid=${req.uid}&token=${rssToken}`;
-		}
-	}
+
+	// single RSS path via helper
+	setRssFields({ data, url, req, rssToken });
 
 	data.filters = helpers.buildFilters(baseUrl, filter, query);
-	data.selectedFilter = data.filters.find(filter => filter && filter.selected);
-	data.terms = helpers.buildTerms(baseUrl, term, query);
-	data.selectedTerm = data.terms.find(term => term && term.selected);
+	data.selectedFilter = data.filters.find(f => f && f.selected);
 
-	const pageCount = Math.max(1, Math.ceil(data.topicCount / settings.topicsPerPage));
-	data.pagination = pagination.create(page, pageCount, req.query);
-	helpers.addLinkTags({
-		url: url,
-		res: req.res,
-		tags: data.pagination.rel,
-		page: page,
-	});
 	return data;
 };
 
-
 require('../promisify')(recentController, ['get']);
+
