0b6068590dcd5ca310ed00a71910fb40dbdf9f13
Salwa Al-Kuwari <skkuwari@andrew.cmu.edu>
2025-10-10 21:59:13 +0000

    chore(lint): use stylistic preset (plugin installed)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,58 +5,65 @@ import publicConfig from 'eslint-config-nodebb/public';
 import commonRules from 'eslint-config-nodebb/common';
 
 import { defineConfig } from 'eslint/config';
-import stylisticJs from '@stylistic/eslint-plugin-js'
 import js from '@eslint/js';
 import globals from 'globals';
+import stylisticJs from '@stylistic/eslint-plugin-js';
 
 export default defineConfig([
-	{
-		ignores: [
-			'node_modules/',
-			'.project',
-			'.vagrant',
-			'.DS_Store',
-			'.tx',
-			'logs/',
-			'public/uploads/',
-			'public/vendor/',
-			'.idea/',
-			'.vscode/',
-			'*.ipr',
-			'*.iws',
-			'coverage/',
-			'build/',
-			'test/files/',
-			'*.min.js',
-			'install/docker/',
-		],
-	},
-	// tests
-	{
-		plugins: { "@stylistic": stylisticJs},
-		extends: ["plugin:@stylistic/recommended"],
-		files: ['test/**/*.js'],
-		languageOptions: {
-			ecmaVersion: 2020,
-			sourceType: 'commonjs',
-			globals: {
-				...globals.node,
-				...globals.browser,
-				it: 'readonly',
-				describe: 'readonly',
-				before: 'readonly',
-				beforeEach: 'readonly',
-				after: 'readonly',
-				afterEach: 'readonly',
-			},
-		},
-		rules: {
-			...commonRules,
-			'no-unused-vars': 'off',
-			'no-prototype-builtins': 'off',
-		}
-	},
-	...publicConfig,
-	...serverConfig
+  // global ignores
+  {
+    ignores: [
+      'node_modules/',
+      '.project',
+      '.vagrant',
+      '.DS_Store',
+      '.tx',
+      'logs/',
+      'public/uploads/',
+      'public/vendor/',
+      '.idea/',
+      '.vscode/',
+      '*.ipr',
+      '*.iws',
+      'coverage/',
+      'build/',
+      'test/files/',
+      '*.min.js',
+      'install/docker/',
+    ],
+  },
+  // tests
+{
+  files: ['test/**/*.js'],
+  plugins: {
+    '@stylistic/js': (await import('@stylistic/eslint-plugin')).default,
+  },
+  extends: [
+    'js/recommended',
+    'plugin:@stylistic/js' // now works because the main plugin is installed
+  ],
+  languageOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'commonjs',
+    globals: {
+      ...globals.node,
+      ...globals.browser,
+      it: 'readonly',
+      describe: 'readonly',
+      before: 'readonly',
+      beforeEach: 'readonly',
+      after: 'readonly',
+      afterEach: 'readonly',
+    },
+  },
+  rules: {
+    ...commonRules,
+    'no-unused-vars': 'off',
+    'no-prototype-builtins': 'off',
+    // if you still see spacing nags in tests, you can disable this:
+    // '@stylistic/js/no-multi-spaces': 'off',
+  },
+},
+  ...publicConfig,
+  ...serverConfig,
 ]);
-
