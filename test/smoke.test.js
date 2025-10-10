bf96153a3651a4cb4fff57b4e6a375324c0e30b9
Salwa Al-Kuwari <skkuwari@andrew.cmu.edu>
2025-10-10 14:08:09 +0000

    style: fix lint indentation to satisfy CI

--- test/smoke.test.js
+++ test/smoke.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 
 describe('CI smoke', () => {
-  it('runs at least one test', () => {
-    assert.strictEqual(1, 1);
-  });
+	it('runs at least one test', () => {
+		assert.strictEqual(1, 1);
+	});
 });
